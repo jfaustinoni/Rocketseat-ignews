c75bea14bf1b1ec9abf792378bad3468a9c89212
Jhonatan Faustinoni <jhonatanwesley.faustinoni@gmail.com>
2021-10-18 08:29:46 -0300

    Header and styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>Home | ig news</title>
+      </Head>
+      <h1>
+        teste
+      </h1>
+    </>
+    
+  )
+}
